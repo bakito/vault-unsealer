e5472aec28655d934eaff0fd5661289338f870d5
Marc Brugger <github@bakito.ch>
2025-12-09 07:26:39 +0100

    Fix syntax in openbao-03-check.sh

--- testdata/e2e/openbao-03-check.sh
+++ testdata/e2e/openbao-03-check.sh
@@ -5,6 +5,7 @@ set -e
 # Variables
 NAMESPACE="openbao"
 
+kubectl get pods -n $NAMESPACE -o wide
 
 PODS=($(kubectl get pods -n $NAMESPACE -l app.kubernetes.io/name=openbao -o jsonpath="{.items[*].metadata.name}"))
 
